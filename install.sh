79b18f2f739be5e18a8321a50bf4998785d6e9fc
nacefguessaymi <nacefguess@hotmail.com>
2026-09-15 13:34:17 -0400

    [fix]: build against boost 1.92 (libboost) via cmake config mode for rdkit compat

--- install.sh
+++ install.sh
@@ -65,6 +65,7 @@ apply_patch() {
 echo "==> [2/5] applying source patches"
 apply_patch "$SMINA_DIR" "$PATCH_DIR/smina/smina-01-filesystem-api.patch"
 apply_patch "$SMINA_DIR" "$PATCH_DIR/smina/smina-02-cxx14.patch"
+apply_patch "$SMINA_DIR" "$PATCH_DIR/smina/smina-03-boost-config-mode.patch"
 apply_patch "$REPO_ROOT" "$PATCH_DIR/pharmit/pharmit-01-cmakelists-no-server.patch"
 apply_patch "$REPO_ROOT" "$PATCH_DIR/pharmit/pharmit-02-main-no-server.patch"
 
